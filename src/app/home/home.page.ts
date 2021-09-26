058ac72f5d44e500bf88e75310c2304bc27413a4
Alex Sander Resende de Deus <prof.alexresende@gmail.com>
2021-09-26 12:10:19 -0300

    App com google maps - Ionic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,52 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  @ViewChild('map') mapRef: ElementRef;
+
+  map: google.maps.Map;
+
+  minhaPosicao: google.maps.LatLng;
+
+  constructor(private geolocation: Geolocation) { }
+
+  ionViewWillEnter() {
+    this.exibirMapa();
+  }
+
+  exibirMapa(): void {
+    const posicao = new google.maps.LatLng(-23.550521, -46.634308);
+    const opcoes = {
+      center: posicao,
+      zoom: 15,
+      disableDefaultUI: false
+    };
+
+    this.map = new google.maps.Map(this.mapRef.nativeElement, opcoes);
+
+    this.buscarPosicao();
+  }
+
+  buscarPosicao() {
+    this.geolocation.getCurrentPosition().then((resp) => {
+      this.minhaPosicao = new google.maps.LatLng(resp.coords.latitude,
+        resp.coords.longitude);
+        this.irParaPosicao();
+    }).catch((error) => {
+      console.log('Error getting location', error);
+    });
+  }
+
+  irParaPosicao() {
+    this.map.setCenter(this.minhaPosicao);
+    this.map.setZoom(15);
+
+    new google.maps.Marker({
+      position: this.minhaPosicao,
+      map: this.map,
+      title: 'Minha Posição',
+      animation: google.maps.Animation.BOUNCE
+    });
+  }
+
 
 }
